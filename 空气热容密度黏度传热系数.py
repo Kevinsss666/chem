b39c1d6ef9188a438db2d39ef3ab59bdb1820fa3
Kevinsss666 <2692243385@qq.com>
2026-03-26 01:48:36 +0800

    Add files via upload

---
+++ 空气热容密度黏度传热系数.py
@@ -0,0 +1,21 @@
+import os
+try:
+    from CoolProp.CoolProp import PropsSI
+except:
+    os.system("pip install CoolProp")
+
+
+while True:
+    T=float(input("请输入温度（℃）："))+273.15
+    P =float(input("请输入压强（kPa）："))*1000+101325
+
+    rho = PropsSI('D','T',T,'P',P,'Air')        #密度
+    cp = PropsSI('C','T',T,'P',P,'Air')         #比热
+    mu = PropsSI('V','T',T,'P',P,'Air')      # 黏度
+    k  = PropsSI('L','T',T,'P',P,'Air')   # 导热系数
+
+
+    print("Cp（kj/kg*℃）:", cp/1000)
+    print("密度（kg/m^3）:", rho)
+    print("粘度（Pa*s）",mu)
+    print("导热系数（W/m*℃）",k)
